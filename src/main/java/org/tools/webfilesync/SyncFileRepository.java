eac81dcc55dc23241fc85a37dca88c23ed3db3ad
abdza <abdullah.zainul@gmail.com>
2021-05-06 13:50:23 +0800

    Added function to delete files

--- src/main/java/org/tools/webfilesync/SyncFileRepository.java
+++ src/main/java/org/tools/webfilesync/SyncFileRepository.java
@@ -1,5 +1,6 @@
 package org.tools.webfilesync;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ public interface SyncFileRepository  extends JpaRepository<SyncFile, Long> {
 
 	SyncFile findOneByPath(String path);
 	List<SyncFile> findAllByOp(String op);
+	List<SyncFile> findAllByLastCheckedNot(Date lastChecked);
 }
--- src/main/java/org/tools/webfilesync/SyncService.java
+++ src/main/java/org/tools/webfilesync/SyncService.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -62,7 +63,7 @@ public class SyncService {
 
     }
 	
-	public void synclocalfile(String basefolder) {			    
+	public void synclocalfile(Date updateDate, String basefolder) {			    
 		System.out.println("Syncing " + basefolder);
 		Path path = Paths.get(basefolder);
 		List<Path> paths;
@@ -83,10 +84,7 @@ public class SyncService {
 							e.printStackTrace();
 						}
 					}
-					else {
-						prevfile.setOp("no-op");
-					}
-					prevfile.setLastChecked(new Date());
+					prevfile.setLastChecked(updateDate);
 					repo.save(prevfile);
 				}
 				else {
@@ -96,7 +94,7 @@ public class SyncService {
 					sfile.setFolderPath(x.getParent().toString());
 					sfile.setRelPath(x.getParent().toString().substring(basefolder.length()));
 					sfile.setLastUpdate(x.toFile().lastModified());
-					sfile.setLastChecked(new Date());
+					sfile.setLastChecked(updateDate);
 					sfile.setOp("upload");
 					
 					try {
@@ -116,18 +114,32 @@ public class SyncService {
 		}
 	}
 	
-	
-	public void syncuploadfile(Long maxsize) {		
+	public void detectDeleted(Date updateDate) {
+		List<SyncFile> ufiles = repo.findAllByLastCheckedNot(updateDate);
 		
+		ufiles.forEach(uf -> {			
+			System.out.println("Checking file " + uf.getName());
+			File curfile = new File(uf.getPath());
+			if(!curfile.exists()) {
+				uf.setOp("delete");
+				uf.setLastChecked(updateDate);
+				repo.save(uf);
+			}
+		});
+	}
+	
+	
+	public void syncuploadfile(Date updateDate, String fileurl, String base64creds, Long maxsize) {		
+		System.out.println("In syncupload");
 		List<SyncFile> ufiles = repo.findAllByOp("upload");
-		String fileurl = "http://localhost:9010/api/file_manager/explore/downloads";
+		
 		ufiles.forEach(uf -> {			
 			System.out.println("Sending " + uf.getName());
 			if(uf.getSize()<maxsize) {
 				RestTemplate restTemplate = new RestTemplate();
 			    HttpHeaders headers = new HttpHeaders();
 			    headers.setContentType(MediaType.MULTIPART_FORM_DATA);
-			    headers.add("authorization", "Basic YWRtaW46Y2JteWQzdiFAIw==");
+			    headers.add("authorization", "Basic " + base64creds);
 			    
 			    MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
 			    body.add("file", new FileSystemResource(new File(uf.getPath())));
@@ -140,6 +152,8 @@ public class SyncService {
 			    
 			    ResponseEntity<String> response = restTemplate.postForEntity(
 			    		fileurl, request , String.class);
+			    uf.setOp("no-op");
+			    repo.save(uf);
 			}
 			else {
 				System.out.println("FIle too big to send");
@@ -147,10 +161,39 @@ public class SyncService {
 		});
 	}
 	
+	public void syncdeletefile(Date updateDate, String fileurl, String base64creds) {		
+		System.out.println("In syncdelete");
+		List<SyncFile> ufiles = repo.findAllByOp("delete");
+		
+		ufiles.forEach(uf -> {			
+			System.out.println("Deleting " + uf.getName());
+			
+			RestTemplate restTemplate = new RestTemplate();
+		    HttpHeaders headers = new HttpHeaders();
+		    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+		    headers.add("authorization", "Basic " + base64creds);
+		    		    
+		    MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
+		    map.add("rel_path", uf.getRelPath());
+		    map.add("filename", uf.getName());
+		    map.add("op", "delete");
+		    
+		    HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+		    
+		    ResponseEntity<String> response = restTemplate.postForEntity(
+		    		fileurl, request , String.class);
+		    uf.setOp("no-op");
+		    repo.save(uf);
+			
+		});
+	}
+	
 	@PostConstruct
 	public void init() {
 		String basefolder = null;
 		Long maxsize = (long) 50000000;
+		String fileurl = "http://localhost:9010/api/file_manager/explore/downloads";
+		Date updateDate = new Date();
 		
 		if(args.containsOption("basefolder")) 
         {
@@ -158,6 +201,18 @@ public class SyncService {
             List<String> values = args.getOptionValues("basefolder");
             try {	            	
 				basefolder = values.get(0);				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+		
+		if(args.containsOption("fileurl")) 
+        {
+            //Get argument values
+            List<String> values = args.getOptionValues("fileurl");
+            try {	            	
+				fileurl = values.get(0);				
 			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -176,9 +231,31 @@ public class SyncService {
 			}
         }
 		
+		String plainCreds = "willie:p@ssword";
+		
+		if(args.containsOption("username") && args.containsOption("password")) 
+        {
+            //Get argument values
+            List<String> unvalues = args.getOptionValues("username");
+            List<String> passvalues = args.getOptionValues("password");
+            try {	            	
+				plainCreds = unvalues.get(0) + ":" + passvalues.get(0);	
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+        }
+		
+		byte[] plainCredsBytes = plainCreds.getBytes();
+		byte[] base64CredsBytes = Base64.getEncoder().encode(plainCredsBytes);
+		//byte[] base64CredsBytes = Base64.encodeBase64(plainCredsBytes);
+		String base64Creds = new String(base64CredsBytes);
+		
 		if(basefolder!=null) {
-			synclocalfile(basefolder);
-			syncuploadfile(maxsize);
+			synclocalfile(updateDate,basefolder);
+			syncuploadfile(updateDate,fileurl, base64Creds, maxsize);
+			detectDeleted(updateDate);
+			syncdeletefile(updateDate,fileurl, base64Creds);
 		}
 	}
 }
